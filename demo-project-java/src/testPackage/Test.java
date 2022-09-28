ace2f6f09220d54624aac18866d7a3218c907a10
Amit Gobare <amitgobare23@gmail.com>
2022-09-28 09:25:02 +0530

    pushing code

--- demo-project-java/src/testPackage/Test.java
+++ demo-project-java/src/testPackage/Test.java
@@ -3,9 +3,8 @@ package testPackage;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("Hello Amit");
-		System.out.println("Hello Amit Again");
+		int a=10;
+		
 	}
 
 }
